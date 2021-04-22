d63872a45f47615bb32e59aba53ee26161f01d4e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-04-22 17:21:43 +0900

    rename

--- autd3-freq-shift-fpga.srcs/sources_1/new/pwm_generator.sv
+++ autd3-freq-shift-fpga.srcs/sources_1/new/pwm_generator.sv
@@ -4,7 +4,7 @@
  * Created Date: 15/12/2020
  * Author: Shun Suzuki
  * -----
- * Last Modified: 12/04/2021
+ * Last Modified: 22/04/2021
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2020 Hapis Lab. All rights reserved.
