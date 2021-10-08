280fc385c0f0bc1f66ce43d8436f373b0fe535cf
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-10-09 03:13:54 +0900

    update

--- autd3-freq-shift-fpga.srcs/sources_1/new/pwm_generator.sv
+++ autd3-freq-shift-fpga.srcs/sources_1/new/pwm_generator.sv
@@ -4,7 +4,7 @@
  * Created Date: 15/12/2020
  * Author: Shun Suzuki
  * -----
- * Last Modified: 26/04/2021
+ * Last Modified: 09/10/2021
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2020 Hapis Lab. All rights reserved.
@@ -23,24 +23,20 @@ module pwm_generator#(
            output var PWM_OUT
        );
 
-assign PWM_OUT = pwm({1'b0, TIME}, {1'b0, CYCLE}, {1'b0, DUTY}, {1'b0, PHASE_DELAY});
+assign PWM_OUT = pwm({1'b0, TIME}, {2'b00, CYCLE}, {1'b0, DUTY}, {1'b0, PHASE_DELAY});
 
 function automatic pwm;
     input [WIDTH:0] t;
-    input [WIDTH:0] c;
-    input [WIDTH:0] d;
-    input [WIDTH:0] s;
-    begin
-        if (s < (d>>1)) begin
-            pwm = (s + c <= t + (d>>1)) | (t < s + (d+1>>1));
-        end
-        else if (s + (d+1>>1) > c) begin
-            pwm = (s <= t + (d>>1)) | (t + c < s + (d+1>>1));
-        end
-        else begin
-            pwm = (s <= t + (d>>1)) & (t < s + (d+1>>1));
-        end
-    end
+    input [WIDTH+1:0] C;
+    input [WIDTH:0] D;
+    input [WIDTH:0] P;
+    logic [WIDTH:0] DL = {1'b0, D[WIDTH:1]};
+    logic [WIDTH:0] DR = {1'b0, D[WIDTH:1]} + {16'h0000, D[0]};
+    logic pwm1 = {1'b0, P} <= {1'b0, t} + {1'b0, DL};
+    logic pwm2 = {1'b0, t} < {1'b0, P} + {1'b0, DR};
+    logic pwm1o = C + {1'b0, P} <= {1'b0, t} + {1'b0, DL};
+    logic pwm2o = C + {1'b0, t} < {1'b0, P} + {1'b0, DR};
+    pwm = (pwm1 & pwm2) | ((pwm2 | pwm1o) & (P < DL)) | ((pwm2o | pwm1) & (C < {1'b0, P} + {1'b0, DR}));
 endfunction
 
 endmodule
